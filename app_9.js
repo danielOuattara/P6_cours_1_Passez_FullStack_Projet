711956aecb63eeced10201905793c0c23ee76cbf
daniel ouattara <daniel.ouattara@gmx.com>
2021-01-04 15:09:37 +0100

    P2C1

---
+++ app_9.js
@@ -0,0 +1,67 @@
+const express = require('express');  // importe 'express'
+const bodyParser = require('body-parser');
+
+const app = express(); //  cree une application express
+const  mongoose = require('mongoose'); // importe Mongoose
+
+mongoose.connect('mongodb+srv://danielboua:**CoplanFX15**@cluster0.vndw3.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.use(bodyParser.json());
+
+
+app.post('/api/stuff', (req, res, next) => { 
+    console.log(req.body);
+    res.status(201).json({
+        message: 'objet créé'
+    });
+});
+
+
+app.use('/api/stuff', (req, res, next) => {
+
+    const stuff = [
+        {
+            _id:'1234',
+            title: 'book',
+            description: '', 
+            imageUrl: '',
+            price: 2900,
+            userId: ''
+        },
+        {
+            _id:'1235',
+            title: 'chair',
+            description: '',
+            imageUrl: '',
+            price: 1500,
+            userId: ''
+        },
+        {
+            _id:'1236',
+            title: 'bicycle',
+            description: '',
+            imageUrl: '',
+            price: 15000,
+            userId: ''
+        }
+    ]; 
+
+    res.status(200).json(stuff);
+
+});
+
+module.exports = app;  //  rend 'app' accessible depuis les autres fichiers du projet
+
