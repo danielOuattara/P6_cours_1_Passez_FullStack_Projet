ac07e1bd1216bcc4825eae7afcc28ec7caf2073c
daniel ouattara <daniel.ouattara@gmx.com>
2021-01-19 22:40:28 +0100

    P3C5: TOKEN --> OK

--- server_20/app.js
+++ server_20/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 app.use('/api/stuff', stuffRoutes)
-app.use('api/auth', userRoutes)
+app.use('/api/auth', userRoutes)
 
 module.exports = app;  //  rend 'app' accessible depuis les autres fichiers du projet
 
--- server_20/controllers/userController.js
+++ server_20/controllers/userController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jsonwebtoken = require('jsonwebtoken');
 const User = require('./../../models/User.js')
 
 
@@ -11,7 +12,6 @@ exports.signup = (req, res, next) => {
                     password: hash
                }
             );
-            
             user.save()
               .then( () => res.status(201).json({message: 'User created !'}))
               .catch( error => res.status(400).json({error}))
@@ -22,14 +22,11 @@ exports.signup = (req, res, next) => {
 
 exports.login = (req, res, next) => {
     User.findOne( {email: req.body.email})
-
         .then( user => {
             if (!user) {
                 return res.status(401).json( {error: 'User not registered'})
             }
-
             bcrypt.compare(req.body.password , user.password)
-
                 .then( valid => {
                     if (!valid) {
                         return res.status(401).json( {error: 'Connexion Not Authorized !'})
@@ -37,18 +34,16 @@ exports.login = (req, res, next) => {
                     res.status(200).json(
                         {
                             userId: user._id,
-                            token: 'TOKEN'
+                            token: jsonwebtoken.sign(
+                                { userId: user._id },
+                                'RANDOM_TOKEN_SECRET',
+                                { expiresIn: '24h' }
+                            )
                         }
                     );
-
                 })
-                .catch( error=> res.status(500).josn( {error}))
-
-
-
+                .catch( error=> res.status(500).json( {error}))
         })
         .catch( error => res.status(500).json( {error}))
-
-
 
 };
